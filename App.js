bbe36511892fe9e28ce153932064c7602c0e81fe
Dan Nguyen <nguyen.danimal@gmail.com>
2023-02-02 11:42:54 -0600

    flex to the containers

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your Fitness Goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
       </View>
     </View>
@@ -20,18 +20,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1, // This will make the app container take up the entire screen
     marginTop: 10,
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1, // This will make the input container take up 1/4 of the screen
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5, // This will make the goals container take up 3/4 of the screen
   },
 })
