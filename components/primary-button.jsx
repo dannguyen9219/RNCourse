4427247b5aa5a667365053c097c1a2f9dee4a5ad
Dan Nguyen <nguyen.danimal@gmail.com>
2023-02-06 11:08:26 -0600

    add visual feedback to buttons

--- components/primary-button.jsx
+++ components/primary-button.jsx
@@ -1,16 +1,51 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native"
 
 
 // Application Screen || Define Exports
 // =================================================================================================
 // =================================================================================================
 export const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {
+    console.log("Button Pressed")
+  }
+
   return (
-    <View>
-      <Text>
-        {children}
-      </Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+        pressed
+          ? [styles.buttonInnerContainer, styles.pressed]
+          : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#581f3b" }}
+      >
+        <Text style={styles.buttonText}>
+          {children}
+        </Text>
+      </Pressable>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#961a58",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+})
--- screens/start-game-screen.jsx
+++ screens/start-game-screen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextInput, Pressable, View } from 'react-native'
+import { TextInput, Pressable, View, StyleSheet } from 'react-native'
 
 
 // Application Screen || Define Imports
@@ -13,10 +13,42 @@ import { PrimaryButton } from '../components/primary-button'
 // =================================================================================================
 export const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#b5658d",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+})
