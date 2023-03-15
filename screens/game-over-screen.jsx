2e1f8df4e796d7597995f63f8bc914a725daf3b6
Dan Nguyen <nguyen.danimal@gmail.com>
2023-03-15 11:08:32 -0500

    Add game over image on screen

--- screens/game-over-screen.jsx
+++ screens/game-over-screen.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text, StyleSheet } from 'react-native'
+
+
+// Application Screen || Define Imports
+// =================================================================================================
+// =================================================================================================
+import { Title } from '../components/title'
+import { COLORS } from '../utilities/colors'
 
 
 // Application Screen || Define Exports
@@ -7,8 +14,39 @@ import { View, Text } from 'react-native'
 // =================================================================================================
 export const GameOverScreen = () => {
   return (
-    <View>
-      <Text>Game Over</Text>
+    <View style={styles.screenContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/game-over.png')}
+        />
+      </View>
+      <Text>
+        The Computer needed X rounds to guess your number Y.
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: COLORS.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+})
